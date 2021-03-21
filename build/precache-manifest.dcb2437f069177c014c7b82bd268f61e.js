6a1da84ad420e241cdf023b2aa941bc2bb8ee519
Igor <igorln96@gmail.com>
2021-03-21 16:43:43 -0300

    change color

---
+++ build/precache-manifest.dcb2437f069177c014c7b82bd268f61e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fe392c0b421d040ce8013cbb50cb7204",
+    "url": "/wallet/index.html"
+  },
+  {
+    "revision": "ddfd1d85fe57f23a4f81",
+    "url": "/wallet/static/css/main.453c298e.chunk.css"
+  },
+  {
+    "revision": "944ab952ccc5b3d2648e",
+    "url": "/wallet/static/js/2.45c146a4.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/wallet/static/js/2.45c146a4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ddfd1d85fe57f23a4f81",
+    "url": "/wallet/static/js/main.45dac0c7.chunk.js"
+  },
+  {
+    "revision": "4fb4cea7ccc09b9511f8",
+    "url": "/wallet/static/js/runtime-main.8f1525cf.js"
+  }
+]);
